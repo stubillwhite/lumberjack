eccd2aa08c192276cff57296128619b148c28c44
Stu W <stubillwhite@gmail.com>
2020-02-24 17:15:02 +0000

    Add REST API

--- project.clj
+++ project.clj
@@ -9,17 +9,33 @@
 
   :repl-options {:port 4555}
 
-  :plugins []
+  :plugins [[lein-ring "0.8.11"]]
 
   :main lumberjack.app
-  
-  :dependencies [[nrepl "0.3.1"]
+
+  :ring { :handler lumberjack.rest/app
+          :nrepl   { :start? true
+                     :port   4555 } }
+
+  :dependencies [;; Core
+                 [nrepl "0.3.1"]
                  [org.clojure/clojure "1.9.0"]
-                 [com.taoensso/timbre "4.10.0"]
                  [org.clojure/tools.trace "0.7.10"]
-                 [com.rpl/specter "1.1.3"]
+                 [com.taoensso/nippy "2.14.0"]
+
+                 ;; Logging
+                 [com.taoensso/timbre "4.10.0"]
+
+                 ;; Components
                  [mount "0.1.16"]
-                 [com.taoensso/nippy "2.14.0"]]
+
+                 ;; REST
+                 [cheshire "5.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-core "1.8.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-jetty-adapter "1.8.0"]
+                 [ring/ring-json "0.5.0"]]
   
   :profiles {:uberjar {:aot :all}
 
---
+++ src/lumberjack/rest.clj
@@ -0,0 +1,68 @@
+(ns lumberjack.rest
+  (:require [compojure.core :refer [context defroutes DELETE GET POST PUT]]
+            [compojure.route :as route]
+            [lumberjack.utils :refer [def-]]
+            [mount.core :refer [defstate]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.defaults :refer [wrap-defaults]]
+            [ring.middleware.json
+             :refer
+             [wrap-json-body wrap-json-params wrap-json-response]]
+            [ring.util.response
+             :refer
+             [content-type created not-found redirect response]]
+            [taoensso.timbre :as timbre]))
+
+(timbre/refer-timbre)
+
+(defn- with-response-defaults
+  ([response]
+    (-> response
+      (content-type "application/json"))))
+
+(defn- find-person [params]
+  (println "WHITE IMPLEMENT ME"))
+
+(defroutes app-routes
+  (context "/person" []
+           (GET  "/"  {params :params} (find-person params)))
+  
+  (GET "/" [] (redirect "/index.html"))
+  (route/resources "/")
+  (route/not-found (not-found "Not found")))
+
+(def default-config
+  { :params { :urlencoded true
+              :keywordize true
+              :nested     true
+              :multipart  true }
+
+    :responses { :not-modified-responses true
+                 :absolute-redirects     true
+                 :content-types          true } })
+
+(defn- wrap-log-request [handler]
+  (fn [req]
+    (debug "Handling request" req)
+    (handler req)))
+
+(def app
+  (-> app-routes
+      (wrap-log-request)
+      (wrap-defaults default-config)
+      (wrap-json-body)
+      (wrap-json-params)
+      (wrap-json-response)
+      (wrap-content-type)))
+
+(def- state (atom {:server nil}))
+
+(defstate server
+  :start (let [server (run-jetty #'lumberjack.rest/app {:port 8080 :join? false})]
+           (swap! state assoc :server server))
+  :stop  (if (:server @state)
+           (do
+             (.stop (:server @state))
+             (swap! state assoc :server nil))))
+
